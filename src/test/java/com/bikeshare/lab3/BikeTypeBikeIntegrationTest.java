d771652a8718a278e38e5ce11be88fb9f75695cd
Jennifer Drewert <jennifer@drewert.se>
2025-10-01 16:05:19 +0400

    lagt till tester i biketype gjort tester i biketypebike

--- src/test/java/com/bikeshare/lab3/BikeTypeBikeIntegrationTest.java
+++ src/test/java/com/bikeshare/lab3/BikeTypeBikeIntegrationTest.java
@@ -1,11 +1,11 @@
 package com.bikeshare.lab3;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
 
 import com.bikeshare.model.Bike;
 import com.bikeshare.model.BikeType;
@@ -14,7 +14,7 @@ import com.bikeshare.model.Bike.BikeStatus;
 public class BikeTypeBikeIntegrationTest {
 
     @Test
-    void testStartRideAvailableBike(){
+    void testStartRideAvailableBike() {
         Bike bike = new Bike("1", Bike.BikeType.STANDARD);
 
         bike.startRide();
@@ -22,29 +22,175 @@ public class BikeTypeBikeIntegrationTest {
         assertEquals(Bike.BikeStatus.IN_USE, bike.getStatus());
 
     }
+
     @Test
-    void testStartRideElectricBikeWithSufficientBattery(){
+    void testStartRideElectricBikeWithSufficientBattery() {
         Bike bike = new Bike("1", Bike.BikeType.ELECTRIC);
         bike.startRide();
 
         assertEquals(Bike.BikeStatus.IN_USE, bike.getStatus());
-        assert(bike.getBatteryLevel()>=10);
-
-
+        assert (bike.getBatteryLevel() >= 10);
     }
+
     @Test
-    void testStartRideBikeInUse(){
-        Bike bike = new Bike("1",Bike.BikeType.STANDARD);
+    void testStartRideWithReservedBike() {
+        Bike bike = new Bike("1", Bike.BikeType.PREMIUM);
+
+        // sätter bike till reserved för att säkerställa att det går att starta en ride
+        // med en resarverad bike
+        bike.reserve();
+        bike.startRide();
+
+        assertEquals(Bike.BikeStatus.IN_USE, bike.getStatus());
+    }
+
+    @Test
+    void testStartRideBikeInUse() {
+        Bike bike = new Bike("1", Bike.BikeType.STANDARD);
         bike.startRide();
         Exception exception = assertThrows(IllegalStateException.class, () -> {
             bike.startRide();
         });
 
-        
         assertEquals("Cannot start ride with bike in status: IN_USE", exception.getMessage());
-
-        
     }
 
-    
+    @Test
+    void testStartRideWithElectricBikeInsufficientBattery() {
+        Bike bike = new Bike("1", Bike.BikeType.ELECTRIC);
+
+        // startar en ride för att få ner batteri nivån. Slutar ride efter 47km eftersom
+        // detta använder 94% av batteriet.
+        bike.startRide();
+        bike.endRide(47);
+
+        // Startar en ny ride med samma bike, nu är batteriet 6%, fångar exeptionmessage
+        Exception exception = assertThrows(IllegalStateException.class, () -> {
+            bike.startRide();
+        });
+
+        assertEquals("Electric bike battery too low to start ride", exception.getMessage());
+    }
+
+    // testar endRide
+    @Test
+    void endRideStandardBike() {
+        Bike bike = new Bike("1", Bike.BikeType.STANDARD);
+        bike.startRide();
+
+        bike.endRide(50);
+        assertEquals(Bike.BikeStatus.AVAILABLE, bike.getStatus());
+    }
+
+    @Test
+    void endRideElectricBike() {
+        Bike bike = new Bike("1", Bike.BikeType.ELECTRIC);
+        bike.startRide();
+
+        bike.endRide(30);
+        assertEquals(40, bike.getBatteryLevel());
+        assertEquals(Bike.BikeStatus.AVAILABLE, bike.getStatus());
+    }
+
+    @Test
+    void endRideAvalibleBike() {
+        Bike bike = new Bike("1", Bike.BikeType.STANDARD);
+
+        assertEquals(Bike.BikeStatus.AVAILABLE, bike.getStatus());
+        Exception exception = assertThrows(IllegalStateException.class, () -> {
+            bike.endRide(30);
+        });
+
+        assertEquals("Cannot end ride for bike not in use", exception.getMessage());
+    }
+
+    @Test
+    void endRideNegativeDistance() {
+        Bike bike = new Bike("1", Bike.BikeType.STANDARD);
+        bike.startRide();
+
+        assertEquals(Bike.BikeStatus.IN_USE, bike.getStatus());
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bike.endRide(-10);
+        });
+
+        assertEquals("Distance traveled cannot be negative", exception.getMessage());
+    }
+
+    // testar om bike får rätt batterinivå
+
+    @Test
+    void batteryLevelStandardBikeIsMinusOne() {
+        Bike bike = new Bike("1", Bike.BikeType.STANDARD);
+
+        assertEquals(-1.0, bike.getBatteryLevel());
+    }
+
+    @Test
+    void batteryLevelPremiunBikeIsMinusOne() {
+        Bike bike = new Bike("1", Bike.BikeType.PREMIUM);
+
+        assertEquals(-1.0, bike.getBatteryLevel());
+    }
+
+    @Test
+    void batteryLevelElectricBikeIs100() {
+        Bike bike = new Bike("1", Bike.BikeType.ELECTRIC);
+
+        assertEquals(100, bike.getBatteryLevel());
+    }
+
+    // testar om charge battery fungerar korrekt, endast electric bike ska ladda
+    @Test
+    void chargeBatteryOnElectricBikeTo80() {
+        Bike bike = new Bike("1", Bike.BikeType.ELECTRIC);
+
+        // startar en ride för att få ner batteri nivån. Slutar ride efter 47km eftersom
+        // detta använder 94% av batteriet.
+        bike.startRide();
+        bike.endRide(47);
+
+        bike.chargeBattery(74);
+
+        assertEquals(80, bike.getBatteryLevel());
+    }
+
+    @Test
+    void chargeBatteryOnElectricBikeToHigh() {
+        Bike bike = new Bike("1", Bike.BikeType.ELECTRIC);
+        // startar en ride för att få ner batteri nivån. Slutar ride efter 47km eftersom
+        // detta använder 94% av batteriet.
+        bike.startRide();
+        bike.endRide(47);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bike.chargeBattery(110);
+        });
+        assertEquals("Charge amount must be between 0 and 100", exception.getMessage());
+    }
+
+    @Test
+    void chargeBatteryOnElectricBikeTooLow() {
+        Bike bike = new Bike("1", Bike.BikeType.ELECTRIC);
+        // startar en ride för att få ner batteri nivån. Slutar ride efter 47km eftersom
+        // detta använder 94% av batteriet.
+        bike.startRide();
+        bike.endRide(47);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            bike.chargeBattery(-20);
+        });
+        assertEquals("Charge amount must be between 0 and 100", exception.getMessage());
+    }
+
+    @Test
+    void chargeBatteryOnStandardBikeExceptionMessage() {
+        Bike bike = new Bike("1", Bike.BikeType.STANDARD);
+
+        Exception exception = assertThrows(IllegalStateException.class, () -> {
+            bike.chargeBattery(80);
+        });
+        assertEquals("Cannot charge non-electric bike", exception.getMessage());
+    }
+
 }
--- src/test/java/com/bikeshare/lab3/BikeTypeStructuralTest.java
+++ src/test/java/com/bikeshare/lab3/BikeTypeStructuralTest.java
@@ -11,48 +11,106 @@ import org.junit.jupiter.api.Test;
 import com.bikeshare.model.BikeType;
 
 public class BikeTypeStructuralTest {
+  // testar alla displayname stämmer överrens
   @Test
-  void testElectricBikeDisplayName(){
-    
+  void testStandardBikeDisplayName() {
+
+    assertEquals("Standard Bike", BikeType.STANDARD.getDisplayName());
+  }
+
+  @Test
+  void testElectricBikeDisplayName() {
+
     assertEquals("Electric Bike", BikeType.ELECTRIC.getDisplayName());
   }
 
   @Test
-  void testGetCargoBikePricePerMinute(){
+  void testMountainBikeDisplayName() {
+
+    assertEquals("Mountain Bike", BikeType.MOUNTAIN.getDisplayName());
+  }
+
+  @Test
+  void testCargoBikeDisplayName() {
+
+    assertEquals("Cargo Bike", BikeType.CARGO.getDisplayName());
+  }
+
+  // testar alla price per minute
+  @Test
+  void testGetStandardBikePricePerMinute() {
+    assertEquals(0.50, BikeType.STANDARD.getPricePerMinute());
+  }
+
+  @Test
+  void testGetElectricBikePricePerMinute() {
+    assertEquals(1.00, BikeType.ELECTRIC.getPricePerMinute());
+  }
+
+  @Test
+  void testGetMountainBikePricePerMinute() {
+    assertEquals(0.70, BikeType.MOUNTAIN.getPricePerMinute());
+  }
+
+  @Test
+  void testGetCargoBikePricePerMinute() {
     assertEquals(1.20, BikeType.CARGO.getPricePerMinute());
   }
 
+  // bara electric är IsElectric==true
   @Test
-  void testIsElectric(){
+  void testIsElectric() {
 
-   assertTrue(BikeType.ELECTRIC.isElectric());
+    assertTrue(BikeType.ELECTRIC.isElectric());
   }
 
   @Test
-  void testIsNotElectric(){
+  void testCargoIsNotElectric() {
     assertFalse(BikeType.CARGO.isElectric());
   }
 
+  // testar alla maxspeed
   @Test
-  void testMaxSpeedForElectric25Kmh(){
+  void testMaxSpeedForStandard25Kmh() {
+    assertEquals(25, BikeType.STANDARD.getMaxSpeedKmh());
+
+  }
+
+  @Test
+  void testMaxSpeedForElectric25Kmh() {
     assertEquals(25, BikeType.ELECTRIC.getMaxSpeedKmh());
 
   }
 
   @Test
-  void testInvalidMaxSpeedForElectric30Kmh(){
+  void testMaxSpeedForMountain30Kmh() {
+    assertEquals(30, BikeType.MOUNTAIN.getMaxSpeedKmh());
+
+  }
+
+  @Test
+  void testMaxSpeedForCargo25Kmh() {
+    assertEquals(20, BikeType.CARGO.getMaxSpeedKmh());
+
+  }
+
+  @Test
+  void testInvalidMaxSpeedForElectric30Kmh() {
 
     assertNotEquals(30, BikeType.ELECTRIC.getMaxSpeedKmh());
   }
 
   @Test
-  void testMountainIsMediumHeavyWeightCatagory(){
+  void testAllBikeHaveCorrectyWeightCatagory() {
+    assertEquals("Light", BikeType.STANDARD.getWeightCategory());
+    assertEquals("Heavy", BikeType.ELECTRIC.getWeightCategory());
     assertEquals("Medium-Heavy", BikeType.MOUNTAIN.getWeightCategory());
+    assertEquals("Very Heavy", BikeType.CARGO.getWeightCategory());
   }
 
   @Test
-  void testMountainIsNotLightWeightCatagory(){
+  void testMountainIsNotLightWeightCatagory() {
     assertNotEquals("Light", BikeType.MOUNTAIN.getWeightCategory());
   }
-    
+
 }
